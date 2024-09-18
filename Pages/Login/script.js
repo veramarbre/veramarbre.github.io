9111002e9503323121093b591679cf6cdbb6e6b5
veramarbre <free.community.1985@gmail.com>
2024-09-18 22:07:33 +0100

    Update script.js

--- Pages/Login/script.js
+++ Pages/Login/script.js
@@ -7,6 +7,8 @@ togglePassword.addEventListener("click", () => {
   const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
   passwordInput.setAttribute("type", type);
   togglePassword.classList.toggle("fa-eye-slash");
+  togglePassword.classList.toggle("fa-eye");
+  togglePassword.style.color = type === "password" ? "var(--input-icon-color)" : "var(--button-color)";
 });
 
 // Example translations object
